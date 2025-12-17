1a1f4f23cd628fc507f792a27c6fe9bfc7cc0911
paprika27 <aeltester.schwede@gmail.com>
2025-12-17 12:11:51 +0100

    further debugging apk

--- android/app/src/main/java/com/paprika27/longevitytracker/MainActivity.java
+++ android/app/src/main/java/com/paprika27/longevitytracker/MainActivity.java
@@ -1,5 +1,21 @@
 package com.paprika27.longevitytracker;
 
+import android.content.pm.ApplicationInfo;
+import android.os.Bundle;
+import android.webkit.WebView;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // During development, enable debugging and clear the cache on each run.
+        boolean isDebuggable = (0 != (getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE));
+        if (isDebuggable) {
+            WebView.setWebContentsDebuggingEnabled(true);
+            getBridge().getWebView().clearCache(true);
+        }
+    }
+}
